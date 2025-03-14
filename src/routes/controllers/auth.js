6a905d609f251b4b9e2ed48baa8af6814e94e41e
Benjamin <natanael.96.01@gmail.com>
2025-03-14 09:11:18 -0300

    added connect to google sheet

--- src/routes/controllers/auth.js
+++ src/routes/controllers/auth.js
@@ -1,32 +1,55 @@
 const { google } = require('googleapis');
 const axios = require("axios");
-const { CLIENT_SECRET, REDIRECT_URL, CLIENT_ID, API_KEY } = process.env;
-
-const drive = google.drive({ 
-    version: 'v3', 
-    auth: API_KEY 
-});
+const { CLIENT_SECRET, REDIRECT_URL, CLIENT_ID, API_KEY, spreadsheetId } = process.env;
 
 const oauth2Client = new google.auth.OAuth2(
     CLIENT_ID,
     CLIENT_SECRET,
     REDIRECT_URL
 );
-const scopes = ['https://www.googleapis.com/auth/drive'];
 
+const scopes = ['https://www.googleapis.com/auth/drive','https://www.googleapis.com/auth/gmail.readonly'];
+
+async function getAccessToken() {
+    try {
+      const tokens = await oauth2Client.getAccessToken();
+      return tokens;
+    } catch (error) {
+      console.error(error);
+    }
+}
+  
 module.exports = {
     generateAuth: async () => {
         // Obtén el código de autorización del usuario
         const authUrl = await oauth2Client.generateAuthUrl({
             access_type: 'offline',
-            scope: scopes
+            scope: scopes.join(" ")
         });
         console.log(authUrl)
         return authUrl;
     },
     redirect: async (code) => {
-        const { tokens } = oauth2Client.getToken(code);
+        const { tokens } = await oauth2Client.getToken(code);
         await oauth2Client.setCredentials(tokens);
         console.log("cuenta vinculada re copado")
+        const drive = google.drive({ 
+            version: 'v3', 
+            auth: oauth2Client 
+        });
+        const sheets = google.sheets('v4')
+        const sheetName = 'Hoja 1';
+        const range = `${sheetName}!A1:B2`;
+        await sheets.spreadsheets.get({
+            spreadsheetId,
+            auth: oauth2Client,
+            range,
+        }, (err, response) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(response.data.values);
+            }
+        });
     }   
 }
