bd63ce64a416b3feba5c1890fd0b23609a62c358
Benjamin <natanael.96.01@gmail.com>
2025-03-27 16:35:02 -0300

    added connection with gmail(inbox)

--- api/src/routes/controllers/auth.js
+++ api/src/routes/controllers/auth.js
@@ -1,6 +1,7 @@
 const { google } = require('googleapis');
 const axios = require("axios");
-const { CLIENT_SECRET, REDIRECT_URL, CLIENT_ID, API_KEY, spreadsheetId } = process.env;
+const { datamigration } = require('googleapis/build/src/apis/datamigration');
+const { CLIENT_SECRET, REDIRECT_URL, CLIENT_ID, spreadsheetId, email_password } = process.env;
 
 const oauth2Client = new google.auth.OAuth2(
     CLIENT_ID,
@@ -15,6 +16,7 @@ const drive = google.drive({
     auth: oauth2Client 
 });
 const sheets = google.sheets('v4')
+const gmail = google.gmail('v1');
 
 async function getAccessToken() {
     try {
@@ -26,6 +28,28 @@ async function getAccessToken() {
 }
   
 module.exports = {
+    get_inbox: async () => {
+      const res = await gmail.users.messages.list({
+        userId: 'me',
+        labelIds: ['INBOX'],
+        auth: oauth2Client,
+      });
+      return res.data.messages;   
+    },
+    get_messages: async (messages) => {
+      let all_messages = [];
+        messages.forEach((message) => {
+        const messageId = message.id;
+        const res = gmail.users.messages.get({
+          userId: 'me',
+          id: messageId,
+          auth: oauth2Client,
+        });
+        all_messages = [...all_messages, res ];
+      }); 
+      all_messages = Promise.all(all_messages).then(res => res);
+      return all_messages;
+    },
     generateAuth: async () => {
         // Obtén el código de autorización del usuario
         const authUrl = await oauth2Client.generateAuthUrl({
@@ -49,14 +73,18 @@ module.exports = {
         });
         return data.data;
     },
-    updateData: async (sheetName, cells, values, valueInputOption) => {
-        const body = { values };
+    updateData: async (sheetName, cells, values) => {
+        values= [ [ values ], ];
         const range = `${sheetName}!${cells}`;
+        const body = {
+            values,
+        };
        await sheets.spreadsheets.values.update({
             spreadsheetId,
-            range,
-            valueInputOption: valueInputOption,
+            valueInputOption: "USER_ENTERED",
             resource: body,
+            range,
+            auth: oauth2Client,
         });
     }
 }
--- api/src/routes/google.js
+++ api/src/routes/google.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { generateAuth, redirect, getData, updateData } = require("./controllers/auth");
+const { 
+    generateAuth, 
+    redirect, 
+    getData, 
+    updateData, 
+    get_inbox,
+    get_messages,
+} = require("./controllers/auth");
 
 router.get("/", async (req,res) => {
     const url = await generateAuth();
@@ -29,6 +36,34 @@ router.get('/getData', async (req, res) => {
         res.status(404).send({ msg: error.message });
     }
 });
+router.get('/get_inbox', async (req, res) => {
+    try {
+        const messages_inbox = await get_inbox();
+        res.status(200).json({
+            msg: "Casilla de entrada abierta...",
+            messages_inbox
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(404).send(error.message)
+    }
+});
+
+router.post('/get_messages', async (req, res) => {
+    try {
+        const { messages } = req.body;
+        let  all_messages = await get_messages(messages);
+        all_messages = all_messages.map(msg => msg.data.payload)
+        all_messages = all_messages.map(msg => msg.parts);
+        res.status(200).json({
+            msg: "Textos listoss...",
+            all_messages
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(404).send(error.message)
+    }
+});
 
 router.put("/update", async (req, res) => {
     try {
@@ -36,7 +71,8 @@ router.put("/update", async (req, res) => {
         await updateData(sheetName, cells, values, valueInputOption);
         res.status(200).json({ msg: "se realizo copado cambio amiguito"});
     } catch(error) {
-        res.status(404).send({ msg: error.message });
+        console.log(error)
+        res.status(404).send(error.message);
     }
 });
 
